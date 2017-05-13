d4a6eadc037b64630aaf03bc826c663893aff5b8
Matt McKenna <matt@mtmckenna.com>
2017-05-13 09:42:13 -0700

    add convas to the component

--- src/ui/components/croissant-shot/component.ts
+++ src/ui/components/croissant-shot/component.ts
@@ -1,5 +1,23 @@
 import Component from "@glimmer/component";
 
 export default class CroissantShot extends Component {
+  score = 0;
 
+  get canvas() {
+    return document.getElementById('game-canvas');
+  }
+
+  get context() {
+    return this.canvas.getContext('2d');
+  }
+
+  draw() {
+    this.context.moveTo(0,0);
+    this.context.lineTo(200,100);
+    this.context.stroke();
+  }
+
+  didInsertElement() {
+    this.draw();
+  }
 }
